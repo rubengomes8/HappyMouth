f6568f05d3cbf0dc04948b166e9ae41c8ac6765c
rubengomes8 <rsantosgomes@ext.worten.pt>
2023-11-02 15:59:02 +0000

    create Text components and colors file

---
+++ src/components/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+    primary: '#222831',
+    secondary: '#393E46',
+    tertiary: 'EEEEEE',
+    accent: '00AD85',
+    darkGray: '#111827',
+    lightGray: '#6B7280',
+    white: '#FFF',
+    black: '#000',
+    success: '#22C55E',
+    fail: '#EF4444',
+}
